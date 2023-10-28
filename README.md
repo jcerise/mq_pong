09f7088c12f4727262ffd310b881f76aa720f5dc
jcerise <jcerise06@gmail.com>
2023-10-28 11:41:44 -0600

    "Finished" game, simple AI, scoring, score keeping, paddle physics

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 ### Macroquad Pong
 
+![Screenshot](pong_screen.png)
+
 Super simple Pong implementation written in Rust, using the Macroquad crate.
 
 Using this to learn the basics of Macroquad, and Rust at the same time. Ignore the bad conventions.
+
+Current limitations:
+- It's impossible to win, as the "AI" is currently perfect
+- No game states, so the game starts and never ends
+- Some bugginess around paddle bounces in certain circumstances
+  - I'm using simple Rect overlaps for collision, as it was fast and easy, so some weirdness occurs
+
+All Assets were created by me for this project. Feel free to use them however you see fit.
--- src/main.rs
+++ src/main.rs
@@ -31,16 +31,19 @@ impl Velocity {
     }
 }
 
+#[derive(Clone, Copy)]
 struct MoveableObject<'a> {
     texture: &'a Texture2D,
     point: Point,
     velocity: Option<Velocity>,
+    rect: Rect
 }
 
 impl MoveableObject<'_> {
     fn update_position(&mut self, x: f32, y: f32) {
         self.point.x = x;
         self.point.y = y;
+        self.rect.move_to(Vec2::new(x, y));
     }
     fn update_velocity(&mut self, vel: Velocity) {
         self.velocity = Option::from(vel);
@@ -64,36 +67,67 @@ async fn main() {
     let divider: Texture2D = load_texture("resources/full_divider.png").await.unwrap();
     let player_paddle: Texture2D = load_texture("resources/player_paddle.png").await.unwrap();
     let opponent_paddle: Texture2D = load_texture("resources/opponent_paddle.png").await.unwrap();
-    let ball: Texture2D = load_texture("resources/ball.png").await.unwrap();
+    let ball_texture: Texture2D = load_texture("resources/ball.png").await.unwrap();
 
+    let player_point = Point::new(10., (screen_height() / 2.) - player_paddle.height());
     let mut player: MoveableObject = MoveableObject {
         texture: &player_paddle,
-        point: Point::new(10., (screen_height() / 2.) - player_paddle.height()),
-        velocity: None
+        point: player_point,
+        velocity: None,
+        rect: Rect::new(player_point.x, player_point.y, player_paddle.width() / 2., player_paddle.height() / 2.)
     };
 
+    let opponent_point = Point::new(screen_width() - 20., (screen_height() / 2.) - opponent_paddle.height());
     let mut opponent: MoveableObject = MoveableObject {
         texture: &opponent_paddle,
-        point: Point::new(screen_width() - 20., (screen_height() / 2.) - opponent_paddle.height()),
-        velocity: None
+        point: opponent_point,
+        velocity: None,
+        rect: Rect::new(opponent_point.x, opponent_point.y, opponent_paddle.width() / 2., opponent_paddle.height() / 2.)
     };
 
+    let ball_point = Point::new(screen_width() / 2. - ball_texture.width(), screen_height() / 2. - ball_texture.height());
     let mut ball: MoveableObject = MoveableObject{
-        texture: &ball,
-        point: Point::new(screen_width() / 2., screen_height() / 2.),
-        velocity: Option::from(Velocity::new(rng.gen_range(0.0..5.0), rng.gen_range(0.0..5.0)))
+        texture: &ball_texture,
+        point: ball_point,
+        velocity: Option::from(Velocity::new(rng.gen_range(0.0..3.0), rng.gen_range(0.0..3.0))),
+        rect: Rect::new(ball_point.x, ball_point.y, ball_texture.width() / 2., ball_texture.height() / 2.)
     };
+
+    let mut player_score = 0;
+    let mut opponent_score = 0;
 
     loop {
         clear_background(BLACK);
 
-        update_ball_position(&mut ball);
+        update_ball_position(&mut ball, player, opponent, &mut player_score, &mut opponent_score);
 
         handle_input(&mut player);
-        move_opponent(&mut player, &mut opponent);
+        move_opponent(&mut ball, &mut opponent);
 
         // The divider is static and does not move, its purpose is to visually divide the screen
         draw_texture(&divider, screen_width() / 2., 0., WHITE);
+
+        // Draw scores
+        draw_text_ex(
+            &player_score.to_string(),
+            100.,
+            30.,
+            TextParams{
+                font_size: 50,
+                color: WHITE,
+                ..Default::default()
+            });
+
+        draw_text_ex(
+            &opponent_score.to_string(),
+            185.,
+            30.,
+            TextParams{
+                font_size: 50,
+                color: WHITE,
+                ..Default::default()
+            });
+
         draw_texture(&player.texture, player.point.x, player.point.y, WHITE);
         draw_texture(&opponent.texture, opponent.point.x, opponent.point.y, WHITE);
 
@@ -108,10 +142,10 @@ async fn main() {
 fn handle_input(p: &mut MoveableObject) {
     let mut delta = 0.;
     if is_key_down(KeyCode::Up) {
-        delta += -2.;
+        delta += -3.;
     }
     if is_key_down(KeyCode::Down) {
-        delta += 2.;
+        delta += 3.;
     }
 
     if (p.point.y + delta) <= 0. || (p.point.y + delta) >= (screen_height() - p.texture.height()) {
@@ -123,23 +157,43 @@ fn handle_input(p: &mut MoveableObject) {
 
 /// Moves the opponent paddle. For now, this simply mirrors the players movement
 /// Intention is for this track the ball
-fn move_opponent(player: &MoveableObject, opponent: &mut MoveableObject) {
-    opponent.update_position(opponent.point.x, player.point.y)
+fn move_opponent(ball: &MoveableObject, opponent: &mut MoveableObject) {
+    opponent.update_position(opponent.point.x, ball.point.y)
 }
 
-fn update_ball_position(ball: &mut MoveableObject) {
+/// Moves the ball, and handles a few distinct cases.
+/// - If the ball has left the right side of the screen, the player has scored, reset the ball and update the players score
+/// - If the ball has left the left side of the screen, the opponent has scored, reset the ball and the opponents score
+/// - If the ball is about to leave the top or bottom of the screen, bounce it instead, inverting its current velocity
+fn update_ball_position(ball: &mut MoveableObject, player_paddle: MoveableObject, opponent_paddle: MoveableObject, player_score: &mut i32, opponent_score: &mut i32) {
     if ball.velocity.is_some() {
         let mut vel = ball.velocity.unwrap();
 
-        if (ball.point.x + vel.x) >= (screen_width() - ball.texture.width() / 2.) || (ball.point.x + vel.x) <= (0. - ball.texture.width() / 2.) {
-            vel.update(-vel.x, vel.y);
+        if (ball.point.x + vel.x) >= (screen_width())  {
+            reset_ball(ball);
+            *player_score += 1;
+        }
+
+        if (ball.point.x + vel.x) <= (0. - ball.texture.width() / 2.) {
+            reset_ball(ball);
+            *opponent_score += 1;
         }
 
         if (ball.point.y + vel.y) <= (0. - ball.texture.width() / 2.) || (ball.point.y + vel.y) >= (screen_height()- ball.texture.width() / 2.) {
             vel.update(vel.x, -vel.y);
         }
 
+        if player_paddle.rect.overlaps(&ball.rect) || opponent_paddle.rect.overlaps(&ball.rect) {
+            vel.update(-vel.x, vel.y);
+        }
+
         ball.update_position(ball.point.x + vel.x, ball.point.y + vel.y);
         ball.update_velocity(vel);
     }
+}
+
+/// Resets the ball to its original position, and its existing velocity
+fn reset_ball(ball: &mut MoveableObject) {
+    let ball_point = Point::new(screen_width() / 2. - ball.texture.width(), screen_height() / 2. - ball.texture.height());
+    ball.update_position(ball_point.x, ball_point.y);
 }
